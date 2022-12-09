791a586592e1f2a6930883f1cda32adf6a69043a
baiyuqi <1353813001@qq.com>
2022-12-09 18:39:02 +0800

    kkk

--- src/components/NftMintor.tsx
+++ src/components/NftMintor.tsx
@@ -9,7 +9,7 @@ import { Button, Checkbox, Form, Input } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import type { NftMeta } from "../service/types";
 import { Upload } from 'antd';
-import {useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import styles from './NftMintor.module.css'
 const props = {
     name: 'file',
@@ -21,12 +21,12 @@ const props = {
 function NftMinter() {
 
     const navigate = useNavigate()
-    const [meta, updateMeta] = useState<NftMeta>({ name: "", descriptipn: "", imageUri:"" })
+    const [meta, updateMeta] = useState<NftMeta>({ name: "", descriptipn: "", imageUri: "" })
     const [uri, setUri] = useState("")
-    const store = async (file:any) => {
+    const store = async (file: any) => {
         try {
-        
-       
+
+
             const imageuri = await imageToArweave(file)//addToIpfs(image)
             messageBox("success", "", imageuri)
             setUri(imageuri);
@@ -38,7 +38,7 @@ function NftMinter() {
 
     const mint = async () => {
         try {
-            const data : NftMeta= { ...meta, imageUri:uri }
+            const data: NftMeta = { ...meta, imageUri: uri }
             const json = JSON.stringify(data);
             const metauri = await toArweave(json)//addToIpfs(json)
             messageBox("success", "", metauri)
@@ -47,7 +47,7 @@ function NftMinter() {
             if (success) {
                 messageBox("success", "", tokenId)
                 navigate("/nft-browser")
-               // router.push("/mynft")
+                // router.push("/mynft")
             } else {
                 messageBox("danger", "", "mint failed")
             }
@@ -61,55 +61,36 @@ function NftMinter() {
     return (
         <div className={styles.CreatorWrapper}>
             <div className={styles.CreatorContainer}>
-                <Form
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    initialValues={{ remember: true }}
-                    //   onFinish={onFinish}
-                    //   onFinishFailed={onFinishFailed}
-                    autoComplete="off"
-                >
-                    <Form.Item
-                        label="name"
-                        name="name"
-                        rules={[{ required: true, message: 'Please input your NFT title!' }]}
-                    >
-                        <Input
-                            placeholder="Asset Name"
-                            //  className={styles.NftField}
-                            onChange={(e) => updateMeta({ ...meta, name: e.target.value })}
 
-                        />
-                    </Form.Item>
+                <Input
+                    placeholder="Asset Name"
+                      className={styles.NftField}
+                    onChange={(e) => updateMeta({ ...meta, name: e.target.value })}
 
-                    <Form.Item
-                        label="description"
-                        name="description"
-                        rules={[{ required: true, message: 'Please input your description!' }]}
-                    >
-                        <Input.TextArea
-                            placeholder="Asset Description"
-                            //  className={styles.NftField}
-                            onChange={(e) => { updateMeta({ ...meta, descriptipn: e.target.value }) }}
-                        />
-                    </Form.Item>
-                    <Input
-                        type='file'
-                        placeholder="Asset Image"
-                        //    className="my-4"
-                        onChange={(e) => { store(e.target.files[0] ) }}
-                    />
-                    
+                />
 
-                    <img width="350" src={uri}  />
 
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" onClick={mint}>
-                            铸币
-                        </Button >
-                    </Form.Item>
-                </Form>
+                <Input.TextArea
+                    placeholder="Asset Description"
+                      className={styles.NftField}
+                    onChange={(e) => { updateMeta({ ...meta, descriptipn: e.target.value }) }}
+                />
+
+                <Input
+                    type='file'
+                    placeholder="Asset Image"
+                    className={styles.NftField}
+                    onChange={(e) => { store(e.target.files[0]) }}
+                />
+
+
+                <img width="350" src={uri} className={styles.NftImage}/>
+
+
+                <Button type="primary" onClick={mint} >
+                    铸币
+                </Button >
+
 
 
             </div>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const NetworkConfiguration = {
     
     chainId: 0x7A69,
-    nftAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+    nftAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
 
     params: [{
         chainId: '0x7A69',
